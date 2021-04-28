56ea278a3214f9ad3992fb38a68877d05bcfa466
SergioAndrade22 <sergio.david2201@gmail.com>
2021-04-28 17:45:15 -0300

    Added theming structure

--- templatefs/src/components/NavBar.vue
+++ templatefs/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar id="nav" toggleable="lg" type="dark" variant="info">
+  <b-navbar id="nav" toggleable="lg" type="dark" variant="app-primary">
     <b-navbar-brand>Family Size</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
--- templatefs/src/main.ts
+++ templatefs/src/main.ts
@@ -2,8 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "@/assets/scss/main.scss";
 import { NavbarPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
